4342f7aa5900851782283ec1c70a25daf36b583f
evanlarsen <evan.larsen@gmail.com>
2017-01-15 23:53:49 -0800

    bug fixing test

--- src/api/games-repo.spec.js
+++ src/api/games-repo.spec.js
@@ -2,9 +2,21 @@
 import {GamesRepository} from './games-repo.inmemory';
 
 describe('gamesRepo', function(){
-    it('getGames should return 3 `games`', function(){
+    it('getGames should return 3 `games`', function(done){
         var repo = new GamesRepository();
-        var games = repo.getGames();
-        expect(games.length).toEqual(3);
+
+        var testGames = function(games){
+            expect(games.length).toEqual(3);
+            done();
+        };
+
+        var failTest = function(err){
+            expect(err).toBeUndefined();
+            done();
+        };
+
+        repo.getGames()
+            .then(testGames)
+            .catch(failTest);
     });
 });
